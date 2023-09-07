b5b9fb61fc3d1a92c5ba8831fc1e00bfc86faf7b
Deshar Sunil <deshario9@gmail.com>
2023-09-07 22:12:40 +0700

    Register assets for absolute path

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import reactLogo from './assets/react.svg'
+import reactLogo from '@assets/react.svg'
 import viteLogo from '/vite.svg'
 import Modal from '@components/Modal'
 import Button from '@components/Button'
--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,14 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      "@components": "/src/components",
-      "@hooks": "/src/hooks",
+      '@hooks': '/src/hooks',
+      '@assets': '/src/assets',
+      '@components': '/src/components',
     },
   },
-});
+})
